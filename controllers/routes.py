86eaa53adf3469e026d6c722632fb75c47594dbb
GABRIEL KENZO TAKEUCHI <gabriel.takeuchi@e239.local>
2025-08-22 11:26:06 -0300

    Finalizado

--- controllers/routes.py
+++ controllers/routes.py
@@ -8,10 +8,22 @@ def init_app(app):
         return render_template('index.html')
 
     @app.route('/api', methods=['GET', 'POST'])
-    def api():
+    @app.route('/api/<string:id>', methods=['GET', 'POST'])
+    def api(id=None):
         url = 'https://ghibliapi.vercel.app/films'
         response = urllib.request.urlopen(url)
         apiData = response.read()
         animeList = json.loads(apiData)
+        if id:
+            animeInfo = []
+            for anime in animeList:
+                if anime['id'] == id:
+                    animeInfo = anime 
+                    break
 
-        return render_template('api.html', animeList=animeList)
+            if animeInfo: 
+                return render_template('apiinfo.html', animeInfo=animeInfo)
+            else:
+                return f'Filme com a ID {id} não foi encontrado.'
+        else:
+            return render_template('api.html', animeList=animeList)
